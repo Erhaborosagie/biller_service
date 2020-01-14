be231261c0364fc9f4ab4ca141ca1e30ab94097d
Osagie Erhabor <erhaborosagie@gmail.com>
2020-01-14 13:25:31 +0100

    Created BillerRepository

---
+++ src/main/java/com/example/biller/repos/BillerRepository.java
@@ -0,0 +1,9 @@
+package com.example.biller.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Osagie Erhabor on 14/01/2020.
+ */
+public interface BillerRepository extends CrudRepository {
+}
